910f7f34bb106b27e4ef4bd3f8a77303207c919d
gloriacuba <gloriajiss@gmail.com>
2021-05-13 12:17:58 -0500

    Agregando Firebase package.Jason

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,19 @@
 // // importamos la funcion que vamos a testear
 global.firebase = jest.fn();
 import { funcionRegistrarse,interfazPrincipal } from '../src/components/intefazPrincipal'
-import {registrarse} from '../src/components/registro.js'
+import {autenticar} from '../src/firebase/firebase.js'
 
-
-describe('registrarse', () => {
-  it('debería ser una función', () => {
-    console.log("test");
-    expect(typeof registrarse).toBe('function');
-  });
-});
+describe('autenticar', () => {
+  test('deberia de registrarme', () => {
+    const promesa = autenticar('ben@example.com', 'examplePass');
+    return promesa
+      .then((user) => {
+        //console.log(user);
+        expect(typeof user).toBe("object");
+        expect(user.email).toBe("ben@example.com")
+      })
+  })
+})
 
 describe('funcionRegistrarse', () => {
   it('debería ser una función', () => {
@@ -17,10 +21,15 @@ describe('funcionRegistrarse', () => {
     expect(typeof funcionRegistrarse).toBe('function');
   });
 });
-
 describe('interfazPrincipal', () => {
   it('debería ser una función', () => {
     console.log("test");
     expect(typeof interfazPrincipal).toBe('function');
   });
 })
+// describe('autenticar', () => {
+//   it('debería ser una función', () => {
+//     console.log("test");
+//     expect(typeof autenticar).toBe('function');
+//   });
+// });
