451b05de44d37165987b0489877904b183d3424e
unknown <lorena0717@hotmail.com>
2021-05-20 11:28:31 -0500

    pruebas test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyChnpSpbN4XUjpjy-cVAXdAhlE8aMNIjX0",
-  authDomain: "social-network-sn9.firebaseapp.com",
-  projectId: "social-network-sn9",
-  storageBucket: "social-network-sn9.appspot.com",
-  messagingSenderId: "227673003549",
-  appId: "1:227673003549:web:c58d79d806e57adb2f58f4",
-  measurementId: "G-NX0STFY82X"
+  apiKey: 'AIzaSyChnpSpbN4XUjpjy-cVAXdAhlE8aMNIjX0',
+  authDomain: 'social-network-sn9.firebaseapp.com',
+  projectId: 'social-network-sn9',
+  storageBucket: 'social-network-sn9.appspot.com',
+  messagingSenderId: '227673003549',
+  appId: '1:227673003549:web:c58d79d806e57adb2f58f4',
+  measurementId: 'G-NX0STFY82X',
 };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,21 @@
+import { ingresoApp } from '../src/components/intefazPrincipal.js';
 import { ingresar } from '../src/firebase/firebase.js';
 
-// const firebasemock = require('firebase-mock');
+const firebasemock = require('firebase-mock');
 
-// const mockauth = new firebasemock.MockAuthentication();
-// const mocksdk = new firebasemock.MockFirebaseSdk(
-//   () => null,
-//   () => mockauth,
-// );
-// mockauth.autoFlush();
-// global.firebase = mocksdk;
+const mockauth = new firebasemock.MockAuthentication();
+const mocksdk = new firebasemock.MockFirebaseSdk(
+  () => null,
+  () => mockauth,
+);
+mockauth.autoFlush();
+global.firebase = mocksdk;
+
+describe('ingresa a iniciar sesion', () => {
+  it('debe ser una funcion', () => {
+    expect(typeof ingresoApp).toBe('function');
+  });
+});
 
 describe('ingresar', () => {
   it('deberia ser una funcion', () => {
@@ -19,7 +26,7 @@ describe('ingresar', () => {
 // describe('autenticar', () => {
 //   test('deberia registrarme', () => {
 //     const promesa = autenticar('petBook@hotmail.com','123456');
-//     console.log('lore');
+//     console.log('probando');
 //     return promesa
 //    .then(()=>{
 //       console.log(algo);
